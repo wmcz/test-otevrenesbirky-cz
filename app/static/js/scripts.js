72ace9b4e6686965d90a4fc7f00e76ad95268dd4
Lukas Pilka <pilka@blueghost.cz>
2021-02-23 17:53:08 +0100

    Updates JS functions

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,20 +1,38 @@
+/* Drawing chart */
 
-let totalArtifactCount = 19007952
-let openArtifactCount = 556280
-let closedArtifactCount = totalArtifactCount - openArtifactCount
-
-let openArtifactPercent = Math.floor(openArtifactCount / totalArtifactCount * 100)
-let closedArtifactPercent = Math.floor(closedArtifactCount / totalArtifactCount * 100)
-
-let openArtifactElement = "<div class='openArtifact'></div>";
-let closedArtifactElement = "<div class='closedArtifact'></div>";
-
-let result = [];
-for (i = 0; i < openArtifactPercent; i++) {
-  result.push(openArtifactElement);
-}
-for (i = 0; i < closedArtifactPercent; i++) {
-  result.push(closedArtifactElement);
+let displayChart = (openArtifactPercent, closedArtifactPercent, totalArtifactCount) => {
+  let openArtifactElement = "<div class='openArtifact'></div>";
+  let closedArtifactElement = "<div class='closedArtifact'></div>";
+  let openedItems = Math.ceil(openArtifactPercent * (totalArtifactCount / 10000))
+  let closedItems = Math.ceil(closedArtifactPercent * (totalArtifactCount / 10000))
+  let result = [];
+  for (i = 0; i < openedItems; i++) {
+    result.push(openArtifactElement);
+  }
+  for (i = 0; i < closedItems; i++) {
+    result.push(closedArtifactElement);
+  }
+  document.getElementById('collectionStats').innerHTML = result.join("");
 }
 
-document.getElementById('collectionStats').innerHTML = result.join("");
+/* Receiving data from select and preparing collection stats*/
+
+let countCollectionData = () => {
+  let collectionValuesStr = document.getElementById("collectionSelect");
+  let collectionValues = collectionValuesStr.value.split(",");
+  let totalArtifactCount = parseInt(collectionValues[0]);
+  let openArtifactCount = parseInt(collectionValues[1]);
+  let closedArtifactCount = totalArtifactCount - openArtifactCount;
+  let openArtifactPercent = openArtifactCount / totalArtifactCount * 100;
+  let closedArtifactPercent = closedArtifactCount / totalArtifactCount * 100;
+
+  console.log('Total: '+ totalArtifactCount)
+  console.log('Open: '+ openArtifactCount + ', '+ openArtifactPercent + '%')
+  console.log('Closed: '+ closedArtifactCount + ', '+ closedArtifactPercent + '%')
+
+  displayChart(openArtifactPercent, closedArtifactPercent, totalArtifactCount);
+}
+
+/* Default load */
+window.onload = countCollectionData;
+
