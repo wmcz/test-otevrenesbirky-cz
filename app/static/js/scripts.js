1addbe8147711e60b419fc809bf2252f3d70ad52
Lukas Pilka <pilka@blueghost.cz>
2021-02-23 22:48:53 +0100

    Updates JS functions

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,18 +1,50 @@
 /* Drawing chart */
 
 let displayChart = (openArtifactPercent, closedArtifactPercent, totalArtifactCount) => {
-  let openArtifactElement = "<div class='openArtifact'></div>";
-  let closedArtifactElement = "<div class='closedArtifact'></div>";
-  let openedItems = Math.ceil(openArtifactPercent * (totalArtifactCount / 10000))
-  let closedItems = Math.ceil(closedArtifactPercent * (totalArtifactCount / 10000))
+  let canvas = document.getElementById('collectionStats');
+  let canvasWidth = canvas.offsetWidth;
+  let canvasHeight = canvas.offsetHeight;
+  console.log('canvas width:'+ canvasWidth);
+  console.log('canvas height:'+ canvasHeight);
+
+  let openedItems = openArtifactPercent * (totalArtifactCount / 10000);
+  let closedItems = closedArtifactPercent * (totalArtifactCount / 10000);
+  let itemsCount = Math.ceil(openedItems + closedItems);
+  let rowItemsCount = Math.ceil(Math.sqrt(itemsCount));
+  if (rowItemsCount < 5){
+    rowItemsCount = 5;
+  }else if (rowItemsCount > 50){
+    rowItemsCount = 50;
+  }
+  let columnItemsCount = Math.ceil(Math.sqrt(itemsCount));
+  if (columnItemsCount < 5){
+    columnItemsCount = 5;
+  }else if (columnItemsCount > 50){
+    columnItemsCount = 50;
+  }
+  totalArtifactCount = rowItemsCount * columnItemsCount
+  console.log('items count:' + itemsCount);
+  console.log('row items count:' + rowItemsCount);
+  console.log('column items count:' + columnItemsCount);
+
+  let itemWidth = canvasWidth / rowItemsCount;
+  let itemHeight = canvasHeight / columnItemsCount;
+  console.log('item width:' + itemWidth);
+  console.log('item height:' + itemHeight);
+
   let result = [];
+  /*
   for (i = 0; i < openedItems; i++) {
     result.push(openArtifactElement);
   }
-  for (i = 0; i < closedItems; i++) {
+  */
+  for (i = 0; i < totalArtifactCount; i++) {
+    let animationDelay = Math.random()*6;
+    let closedArtifactElement = `<div class='closedArtifact artifactItem' style='width: ${itemWidth}; height: ${itemHeight}; animation-delay: ${animationDelay}s'></div>`;
+    console.log(animationDelay);
     result.push(closedArtifactElement);
   }
-  document.getElementById('collectionStats').innerHTML = result.join("");
+  canvas.innerHTML = result.join("");
 }
 
 /* Receiving data from select and preparing collection stats*/
@@ -35,4 +67,5 @@ let countCollectionData = () => {
 
 /* Default load */
 window.onload = countCollectionData;
+window.onresize = countCollectionData;
 
