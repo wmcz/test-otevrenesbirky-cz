c182e2c81eff8f50ca90477eaab8fd333927a906
Martin Urbanec <martin.urbanec@wikimedia.cz>
2023-04-27 14:56:50 +0200

    OTEVRENESBIRKY_PRODUCTION: Add default value

--- app/context_processor.py
+++ app/context_processor.py
@@ -4,5 +4,5 @@ import os
 @app.context_processor
 def inject_variables():
     return {
-        'isProduction': os.environ.get('OTEVRENESBIRKY_PRODUCTION').lower() == 'true'
+        'isProduction': os.environ.get('OTEVRENESBIRKY_PRODUCTION', 'false').lower() == 'true'
     }
