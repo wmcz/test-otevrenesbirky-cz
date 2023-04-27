9f9ec92ebe4ed0afd0bc9b8a3af494b5d568a8e2
Martin Urbanec <martin.urbanec@wikimedia.cz>
2023-04-27 14:50:55 +0200

    Clearly denote a testing deployment

---
+++ app/context_processor.py
@@ -0,0 +1,8 @@
+from app import app
+import os
+
+@app.context_processor
+def inject_variables():
+    return {
+        'isProduction': os.environ.get('OTEVRENESBIRKY_PRODUCTION').lower() == 'true'
+    }
