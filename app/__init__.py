c077d2701242f3e89417dc628ac4763d90040f27
Martin Urbanec <martin.urbanec@wikimedia.cz>
2023-05-06 12:41:12 +0200

    Load config in __init__ rather than in individual requests

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
 
+import yaml
+
 app = Flask(__name__)
+
+with open(r'config.yaml') as configFile:
+    app.config.update(yaml.safe_load(configFile))
 
 from app import context_processor, routes, logger
