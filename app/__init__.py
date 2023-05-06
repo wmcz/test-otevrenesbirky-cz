4f776bbb2c714a7024cd5355e4edde70a578b95a
Martin Urbanec <martin.urbanec@wikimedia.cz>
2023-05-06 12:48:03 +0200

    Cache Google Spreadsheet-provided data

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_caching import Cache
 
 import yaml
 
@@ -6,5 +7,7 @@ app = Flask(__name__)
 
 with open(r'config.yaml') as configFile:
     app.config.update(yaml.safe_load(configFile))
+
+cache = Cache(app)
 
 from app import context_processor, routes, logger
